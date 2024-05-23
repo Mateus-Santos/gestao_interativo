12ed7a945c02c2de5294c95820e6949ec4499a4c
Mateus-Santos <mateusantosms11@gmail.com>
2024-05-23 17:02:53 -0300

    Adicionado código de contagem de vagas

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
 from formatacao_cabecalho import mesclarPlanilhas
+from vagas import quadroVagas
 
 mesclarPlanilhas()
+quadroVagas()
--- vagas.py
+++ vagas.py
@@ -1,41 +1,32 @@
 import pandas as pd
 
-from datetime import time
-
-from formatacao_cabecalho import alunosLista
-
-def QuadroVagas():
+def quadroVagas():
     base = pd.read_excel("./bases/Lista geral.xlsx") # Base de dados geral.
-    dias = ["SEG", "TER", "QUA", "QUI", "SEXT", "SAB"]
+    base['HORÁRIO ÍNICIO'] = pd.to_datetime(base['HORÁRIO ÍNICIO'], format='%H:%M:%S', errors='coerce')
+    base['HORARIO FIM'] = pd.to_datetime(base['HORARIO FIM'], format='%H:%M:%S', errors='coerce')
 
-    df = pd.DataFrame({'HORÁRIO ÍNICIO': ['8:00', '10:00', '12:00', '14:00', '16:00'],
-                       'horario_fim': ['10:00', '12:00', '14:00', '16:00', '18:00'],
-                       'SEG':[0,0,0,0,0],
-                       'TER':[0,0,0,0,0],
-                       'QUA':[0,0,0,0,0],
-                       'QUI':[0,0,0,0,0],
-                       'SEXT':[0,0,0,0,0],
-                       'SAB':[0,0,0,0,0]})
+    #Intervalos de tempo para analisar.
+    quadro = {
+        'HORÁRIO ÍNICIO': ['08:00:00', '10:00:00', '12:00:00', '14:00:00', '16:00:00'],
+        'HORARIO FIM': ['10:00:00', '12:00:00', '14:00:00', '16:00:00', '18:00:00'],
+        'SEG': [0, 0, 0, 0, 0],
+        'TER': [0, 0, 0, 0, 0],
+        'QUA': [0, 0, 0, 0, 0],
+        'QUI': [0, 0, 0, 0, 0],
+        'SEXT': [0, 0, 0, 0, 0],
+        'SAB': [0, 0, 0, 0, 0],
+    }
 
-    for dia in dias:
-        dia_escolhido = base[base['DIA'] == dia]
+    quadro_vagas = pd.DataFrame(quadro)
 
-        horario1 = dia_escolhido[dia_escolhido['HORÁRIO ÍNICIO'].apply(lambda x: time(8, 0) <= x < time(10, 00))]
-        vagas1 = horario1.shape[0]
+    quadro_vagas['HORÁRIO ÍNICIO'] = pd.to_datetime(quadro_vagas['HORÁRIO ÍNICIO'], format='%H:%M:%S', errors='coerce')
+    quadro_vagas['HORARIO FIM'] = pd.to_datetime(quadro_vagas['HORARIO FIM'], format='%H:%M:%S', errors='coerce')
 
+    print(quadro_vagas)
 
-        horario2 = dia_escolhido[dia_escolhido['HORÁRIO ÍNICIO'].apply(lambda x: time(10, 0) <= x < time(12, 00))]
-        vagas2 = horario2.shape[0]
+    for dia in range(2, len(quadro_vagas.columns)):
+        for horario in range(len(quadro_vagas[quadro_vagas.columns[dia]])):
+            quadro_vagas[quadro_vagas.columns[dia]][horario] = (base.loc[(quadro_vagas.columns[dia] == base['DIA']) & (quadro_vagas['HORÁRIO ÍNICIO'][horario] >= base['HORÁRIO ÍNICIO']) & (quadro_vagas['HORARIO FIM'][horario] <= base['HORARIO FIM'])].value_counts().sum()) + (base.loc[(quadro_vagas.columns[dia] == base['DIA']) & (base['HORÁRIO ÍNICIO'] < quadro_vagas['HORARIO FIM'][horario]) & (base['HORÁRIO ÍNICIO'] > quadro_vagas['HORÁRIO ÍNICIO'][horario])].value_counts().sum()) + (base.loc[(quadro_vagas.columns[dia] == base['DIA']) & (base['HORARIO FIM'] > quadro_vagas['HORÁRIO ÍNICIO'][horario]) & (base['HORARIO FIM'] < quadro_vagas['HORARIO FIM'][horario])].value_counts().sum())
+    quadro_vagas
 
-        horario3 = dia_escolhido[dia_escolhido['HORÁRIO ÍNICIO'].apply(lambda x: time(12, 0) <= x < time(14, 00))]
-        vagas3 = horario3.shape[0]
-
-        horario4 = dia_escolhido[dia_escolhido['HORÁRIO ÍNICIO'].apply(lambda x: time(14, 0) <= x < time(16, 00))]
-        vagas4 = horario4.shape[0]
-
-        horario5 = dia_escolhido[dia_escolhido['HORÁRIO ÍNICIO'].apply(lambda x: time(16, 0) <= x < time(18, 00))]
-        vagas5 = horario5.shape[0]
-
-        print(vagas1, vagas2, vagas3, vagas4, vagas5)
-
-QuadroVagas()
+    quadro_vagas.to_excel("./bases/QuadroVagas.xlsx", index=False, sheet_name='Assinar')
